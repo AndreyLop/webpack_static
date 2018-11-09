6190699a5522350df9830b9129e69c6a0639eb06
Andrew <latin601@gmail.com>
2018-11-09 14:13:29 +0200

    Init commit

---
+++ webpack.config.js
@@ -0,0 +1,88 @@
+const UglifyJsPlugin = require("uglifyjs-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const OptimizeCSSAssetsPlugin = require("optimize-css-assets-webpack-plugin");
+const autoprefixer = require('autoprefixer');
+
+const apacheServerAddress = 'http://127.0.0.7:80';
+
+module.exports = {
+    mode: 'development',
+    devtool: 'source-map',
+    entry: "./src/js/app.js",
+    output: {
+        publicPath: apacheServerAddress,
+        path: __dirname + "/dist",
+        filename: "js/app.js"
+    },
+    optimization: {
+        minimizer: [
+          new UglifyJsPlugin({
+            cache: true,
+            parallel: true,
+            sourceMap: true // set to true if you want JS source maps
+          }),
+          new OptimizeCSSAssetsPlugin({
+            cssProcessorOptions: {
+              map: {
+                inline: false
+              }
+            }
+          })
+        ]
+    },
+    plugins: [
+        new MiniCssExtractPlugin({
+          filename:"css/styles.css"
+        })
+    ],
+    devServer: {
+        contentBase: [ './dist' ],
+        watchContentBase: true
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js/,
+                exclude: /(node_modules|bower_components)/,
+                use: [{
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/preset-env']
+                    }
+                }]
+            },
+            {
+                test: /\.(sa|sc|c)ss$/,
+                use: [
+                    {
+                        loader: MiniCssExtractPlugin.loader
+                    },
+                    {
+                        loader: 'css-loader', 
+                        options: {
+                            sourceMap: true
+                        }
+                    },
+                    {
+                        loader: 'postcss-loader',
+                        options: {
+                            plugins: [
+                                autoprefixer({
+                                    browsers:['ie >= 8', 'last 4 version']
+                                })
+                            ],
+                            sourceMap: true
+                        }
+                    },
+                    {
+                        loader: 'sass-loader',
+                        options: {
+                            sourceMap: true
+                        }
+                    }
+                  
+                ],
+            }
+        ]
+    }
+}
